ce09e6225ef2d5e7a9637be9dba1b105203a00c7
ad <nanasisine@gmail.com>
2024-05-28 08:49:23 +0900

    rdy

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import "../app/globals.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Button>Secondary</Button>
+    <Input type="email" placeholder="Email" />
+    <p className="text-blue-600 font-bold text-5xl ">
+              Hello Tailwind
+            </p>
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
+
+export default App;
