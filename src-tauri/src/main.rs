5a867b81580ab55c068e16a9a63933834f3f70b9
ad <nanasisine@gmail.com>
2024-05-29 08:32:48 +0900

    テｓｔ

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,47 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+
+fn db_connect() -> Result<sqlite::Connection, sqlite::Error> {
+    let connection = sqlite::open("./test.sqlite");
+    return connection;
+}
+
+fn db_init() -> Result<i32, String> {
+
+    let connection = db_connect().unwrap();
+
+    let query = "
+    CREATE TABLE templates (name TEXT, content TEXT);
+    INSERT INTO templates VALUES ('test1', 'aaaaaaa');
+    INSERT INTO templates VALUES ('test2', 'bbbbbb');";
+
+    let err = connection.execute(query);
+
+    println!("{}", err.is_err().to_string());
+
+    Ok(0)
+}
+
+fn db_all(connection: sqlite::Connection) {
+    
+    let query = "SELECT * FROM templates";
+
+    connection
+        .iterate(query, |pairs| {
+            for &(name, value) in pairs.iter() {
+                println!("{} = {}", name, value.unwrap());
+            }
+            true
+        })
+        .unwrap();
+}
+
+
 fn main() {
+
+    db_init();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { invoke } from "@tauri-apps/api/tauri";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import "../app/globals.css";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "./components/ui/card";
+import { Label } from "./components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "./components/ui/navigation-menu";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -14,29 +18,95 @@ function App() {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-    <Button>Secondary</Button>
-    <Input type="email" placeholder="Email" />
-    <p className="text-blue-600 font-bold text-5xl ">
-              Hello Tailwind
-            </p>
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
 
-      <p>{greetMsg}</p>
+    <div className="grid grid-rows-[auto_1fr] w-screen h-screen">
+      <div><NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <NavigationMenuLink><Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="password">Password</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Account</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" defaultValue="Pedro Duarte" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" defaultValue="@peduarte" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, you'll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Current password</Label>
+              <Input id="current" type="password" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">New password</Label>
+              <Input id="new" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs></NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu></div>
+      <div></div>
     </div>
+
+    // <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    // <Button variant={"outline"}>Secondary</Button>
+    // <Input type="email" placeholder="Email" />
+    // <p className="text-blue-600 font-bold text-5xl ">
+    //           Hello Tailwind
+    //         </p>
+    //   <form
+    //     className="row"
+    //     onSubmit={(e) => {
+    //       e.preventDefault();
+    //       greet();
+    //     }}
+    //   >
+    //     <input
+    //       id="greet-input"
+    //       onChange={(e) => setName(e.currentTarget.value)}
+    //       placeholder="Enter a name..."
+    //     />
+    //     <button type="submit">Greet</button>
+    //   </form>
+
+    //   <p>{greetMsg}</p>
+    // </div>
   );
 }
 
